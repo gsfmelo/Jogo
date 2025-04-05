274d7bc47d9d511e7d3ac280768fa691ce42c188
Geovanna Melo <gsfmelo@gmail.com>
2025-04-05 01:03:20 -0300

    Creating menu

---
+++ assets/Const.py
@@ -0,0 +1,11 @@
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+MENU_OPTION = ('NEW GAME',
+               'NEW GAME - COOPERATIVE',
+               'NEW GAME - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
