9f11cfc8b08ea69023383746cb15b4d3e0270d73
Geovanna Melo <gsfmelo@gmail.com>
2025-04-05 23:17:42 -0300

    Creating collisions, HUD and Score

--- code/Const.py
+++ code/Const.py
@@ -1,8 +1,46 @@
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (255, 200, 0)
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 200, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
+
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Player1': 1,
+    'Player2': 1,
+    'Player1Shot': 25,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy2': 1,
+    'Enemy3': 1,
+    'Enemy1Shot': 20,
+    'Enemy2Shot': 15,
+    'Enemy3Shot': 15
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Player1': 0,
+    'Player2': 0,
+    'Player1Shot': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy2': 125,
+    'Enemy3': 130,
+    'Enemy1Shot': 0,
+    'Enemy2Shot': 0,
+    'Enemy3Shot': 0
+}
 
 ENTITY_HEALTH = {
     'Level1Bg0': 999,
--- code/Menu.py
+++ code/Menu.py
@@ -3,7 +3,7 @@ from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
 
 
 class Menu:
@@ -20,15 +20,15 @@ class Menu:
 
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Crystal", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Crystal", C_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
 
                 else:
-                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
 
             for event in pygame.event.get():
